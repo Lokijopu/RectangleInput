4dd32b53bbb9d1dc65291c652c325307cf6d12e4
Lokijopu <kguo2134@gmail.com>
2018-10-02 13:38:12 -0400

    Add files via upload

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/**
+ * 
+ * Kevin Guo
+ * Period 6 Rectangle
+ */
+public class Rectangle {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Side length of one width of rectange:/t");
+		double width = in.nextDouble();
+		System.out.print("Side length of one height of rectange:/t");
+		double height = in.nextDouble();
+		
+	}
+
+}
